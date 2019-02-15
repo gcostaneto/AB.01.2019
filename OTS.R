6c8f01063edda603f8776a6334cb2fca6e7674b0
Germano Costa Neto <germano.cneto@gmail.com>
2019-02-15 17:40:23 -0200

    Update OTS.R

--- OTS.R
+++ OTS.R
@@ -3,13 +3,15 @@
 #'----------------------------------------------------------------------------------------------
 
 
-OTS <- function(G,Ne,mc.cores=4){
+OTS <- function(G,Ne,mc.cores=4,make.svd=TRUE){
   library(STPGA)
   cat(paste("#----------------------------------------------------------------------------#","\n",sep=""))
   cat(paste("Starting:",Sys.time(),"\n",sep=" "))
+  if(make.svd==TRUE){
   svdG <- svd(G, nu = nrow(G), nv = ncol(G))
   pcsG <- G %*% svdG$v
-  rownames(pcsG) <- rownames(G)
+  rownames(pcsG) <- rownames(G)}
+  if(make.svd==FALSE){pcsG <- G }
   
   TS <- GenAlgForSubsetSelectionNoTest(P = as.matrix(pcsG), ntoselect = Ne,plotiters = F, 
                                        lambda = 1e-5, errorstat = "PEVMEAN", mc.cores = mc.cores)[[1]]
